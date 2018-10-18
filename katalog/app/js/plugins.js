3d1bf984609a80980dfe0bcc494d83e911269690
Vladimir Shabalin <vladimir.shabalin@anychart.com>
2018-10-18 12:55:40 +0800

    5 carusel

--- katalog/app/js/plugins.js
+++ katalog/app/js/plugins.js
@@ -1,23 +1,22 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $(".owl-carousel").owlCarousel({
-        loop:true,
-        margin:10,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-                nav:true,
-                loop:true
+        loop: true,
+        margin: 10,
+        center:true,
+        autoWidth: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true,
             },
-            960:{
-                items:4,
-                nav:true,
-                loop:true
+            960: {
+                items: 4,
+                nav: true,
             },
-            1280:{
-                items:5,
-                nav:true,
-                loop:true
+            1280: {
+                items: 7,
+                nav: true,
             }
         }
     });
